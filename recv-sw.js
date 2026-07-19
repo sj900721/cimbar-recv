36c822679448e3346389d4a0d28820e93b5ecfbe
I563086 <summer.lu@sap.com>
2026-07-19 14:46:29 +0800

    deploy cimbar receiver PWA

---
+++ recv-sw.js
@@ -0,0 +1,41 @@
+var _cacheName = 'cimbar-transfer-recv-v1';
+var _cacheFiles = [
+  'recv.html',
+  'recv-worker.js',
+  'recv-sw.js',
+  'vendor/cimbar_js.js',
+  'vendor/cimbar_js.wasm',
+  'vendor/cimbar_js.2026-01-20T0312.wasm',
+  'vendor/zstd.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(_cacheName).then(function(cache) {
+      return cache.addAll(_cacheFiles);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.map(function(cn) {
+          if (cn !== _cacheName) return caches.delete(cn);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
